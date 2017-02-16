871e72836d59bfdca77cd0573a8ef6bd79476365
Daniel Tahara <tahara@dropbox.com>
2017-02-16 12:17:29 -0800

    Get Cp by name

--- congressperson.py
+++ congressperson.py
@@ -1,14 +1,17 @@
 from datasources import propublica
 from datasources import twitter
+import congress
 
 class Congressperson(object):
+    @classmethod
+    def by_name(cls, cg, name):
+        members = congress.Congress(cg).search_members(name)
+        return cls(members[0]['id']) if len(members) == 1 else None
+
     def __init__(self, id):
         self.pp = propublica.ProPublica()
         self.id = id
         self.member = self.pp.get_member_by_id(id)
-
-    def _get_twitter(self):
-        handle = self.member['twitter_account']
 
     def get_name(self):
         return " ".join([self.member['first_name'],
@@ -18,8 +21,14 @@ class Congressperson(object):
         """
         Get votes by congressperson.
 
-        @param last_n: if > 0, limit on number of votes to return
+        @param last_n: if > 0, limit on number of votes to return. Else returns 100.
         @return: Votes by congressperson.
         """
         votes = self.pp.get_member_votes(self.id)
         return votes[:last_n] if last_n > 0 else votes
+
+    def get_recent_tweets(self):
+        handle = self.member['twitter_account']
+
+    def get_calendar(self):
+        pass
--- indivisible.py
+++ indivisible.py
@@ -1,7 +1,9 @@
-import propublica
-import congress
+import congressperson
+from datasources import propublica
+import pprint
 
 if __name__ == '__main__':
     propublica.ProPublica.load_api_key()
 
-    cg = congress.Congress(115)
+    cp = congressperson.Congressperson.by_name(115, "kamala harris")
+    pprint.pprint(cp.get_recent_votes(5))

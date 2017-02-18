94f037f9fc149d58ee3b50bc97325331eccfd304
Daniel Tahara <tahara@dropbox.com>
2017-02-17 16:33:24 -0800

    Use concepts

--- indivisible/datasources/eventregistry.py
+++ indivisible/datasources/eventregistry.py
@@ -14,20 +14,28 @@ class EventRegistry(object):
     def __init__(self, key):
         self.key = key
 
-    def get_events(self, keyword):
+    def get_concept_uri(self, thing):
+        results = self._get("suggestConcepts", params=dict(prefix=thing))
+        return results[0]['uri'] if len(results) > 0 else None
+
+    def get_events(self, thing):
         params = {
-            "keywords": keyword,
-            "lang": "eng",
             "resultType": "events",
             "action": "getEvents",
         }
+        concept_uri = self.get_concept_uri(thing)
+        if concept_uri:
+            params['conceptUri'] = concept_uri
+        else:
+            params['keywords'] = thing
         results = self._get("event", params=params)
-        return results['events']['results'] if results else []
+        return results['events']['results'] if results and 'events' in results else []
 
     def _get(self, path, params={}, headers={}):
         url = urljoin(self.get_base_url(), path)
         params=params.copy()
         params['apiKey'] = self.key
+        params['lang'] = 'eng'
 
         resp = requests.get(url, params, headers=headers)
         if resp.status_code != 200:

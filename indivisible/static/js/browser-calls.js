d1310761cb65c4d047638224944fe043b5895f93
Daniel Tahara <tahara@dropbox.com>
2017-02-23 00:11:04 -0800

    Clean up js

--- indivisible/static/js/browser-calls.js
+++ indivisible/static/js/browser-calls.js
@@ -2,14 +2,9 @@
  * Twilio Client configuration
  */
 
-// Store some selectors for elements we'll reuse
-var callStatus = $("#call-status");
-var hangUpButton = $(".hangup-button");
-var callCongresspersonButtons = $(".call-congressperson-button");
-
 /* Helper function to update the call status bar */
 function updateCallStatus(status) {
-    callStatus.text(status);
+    $("#call-status").text(status);
 }
 
 /* Get a Twilio Client token with an AJAX request */
@@ -33,8 +28,8 @@ Twilio.Device.error(function (error) {
 /* Callback for when Twilio Client initiates a new connection */
 Twilio.Device.connect(function (connection) {
     // Enable the hang up button and disable the call buttons
-    hangUpButton.prop("disabled", false);
-    callCongresspersonButtons.prop("disabled", true);
+    $(".hangup-button").prop("disabled", false);
+    $(".call-congressperson-button").prop("disabled", true);
 
     updateCallStatus("In call with " + connection.message.phoneNumber);
 });
@@ -42,8 +37,8 @@ Twilio.Device.connect(function (connection) {
 /* Callback for when a call ends */
 Twilio.Device.disconnect(function(connection) {
     // Disable the hangup button and enable the call buttons
-    hangUpButton.prop("disabled", true);
-    callCongresspersonButtons.prop("disabled", false);
+    $(".hangup-button").prop("disabled", true);
+    $(".call-congressperson-button").prop("disabled", false);
 
     updateCallStatus("Ready");
 });
